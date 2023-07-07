5b8168a5099859787ff7ff6e9361f9ccae84bb3b
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-07 22:31:07 +0000

    initial commit

---
+++ src/main/java/com/emma_ea/chat_room/SpringChatroomApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.chat_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emma_ea/chat_room/SpringChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_ea.chat_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
