f504d55f91f677494e2432b53aa62338f51f4fa8
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-09 02:38:05 +0000

    build chat page. wip logout controller

--- src/main/java/com/emma_ea/chat_room/controller/UserController.java
+++ src/main/java/com/emma_ea/chat_room/controller/UserController.java
@@ -2,8 +2,10 @@ package com.emma_ea.chat_room.controller;
 
 import com.emma_ea.chat_room.service.UserService;
 import com.emma_ea.chat_room.exceptions.UserNameAlreadyTaken;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,9 +31,16 @@ public class UserController implements WebMvcConfigurer {
     }
 
     @PostMapping("login")
-    public String loginUser(@RequestBody String userName) throws UserNameAlreadyTaken {
-        service.login(userName);
+    public String loginUser(@RequestBody String userName, HttpServletRequest request) throws UserNameAlreadyTaken {
+        String name = userName.split("=")[1];
+        service.login(name);
+        request.getSession().getServletContext().setAttribute("username", name);
         return "redirect:/chat";
+    }
+
+    @GetMapping("logout")
+    public String logout() {
+        return "/";
     }
 
 }
--- src/main/java/com/emma_ea/chat_room/exceptions/UserNameAlreadyTaken.java
+++ src/main/java/com/emma_ea/chat_room/exceptions/UserNameAlreadyTaken.java
@@ -3,6 +3,6 @@ package com.emma_ea.chat_room.exceptions;
 public class UserNameAlreadyTaken extends AppExceptions {
 
     public UserNameAlreadyTaken(String msg) {
-        super(msg);
+        super(String.format("User name >> %s << is already taken \n", msg));
     }
 }
